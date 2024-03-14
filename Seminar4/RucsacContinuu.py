ef0141543bbf741d07bb4cd1fb955064f262f2bc
Serban Timofte <serbantimofte@MacBook-Pro.local>
2024-03-14 14:50:18 +0200

    Update function to handle edge cases

--- Seminar4/RucsacContinuu.py
+++ Seminar4/RucsacContinuu.py
@@ -42,22 +42,28 @@ def m_neuniforma(gena, sigma, a, b):    # a,b inteval de definire a genelor
 def mutatie_populatie(pop, dim, n, c, v, cmax, probabilitate_mutatie, sigma):
     pop_muntanta = pop.copy()   #deep-copy
     for i in range(dim):
-        individ = pop[i][0:n].copy   #excludem ultima coloana cu fitness-ul => extragem genele
+        individ = pop[i][:n].copy   #excludem ultima coloana cu fitness-ul => extragem genele
         for j in range(n):  #n = numarul de gene
             r = numpy.random.uniform(0, 1)
-            if(r < probabilitate_mutatie):
+            if(r <= probabilitate_mutatie):
                 individ[j] = m_neuniforma(individ[j], sigma, 0, 1)
         cost, fitness = ok(individ, n, c, v, cmax)
         if(cost):
-            indiv = indiv + [fitness]
-            pop_muntanta[i] = indiv.copy()
+            individ = individ + [fitness]
+            pop_muntanta[i] = individ.copy()
     return pop_muntanta
 
 #How to run the code? Follow these instructions:
 '''
 import RucsacContinuu
-pop, dim, n , c, v, cmax = RucsacContinuu.mutatie_populatie(pop, dim, n, c, v, cmax, 0.2, 0.25)
+pop, dim, n , c, v, cmax = RucsacContinuu.gen(30,10)
+pop_m = RucsacContinuu.mutatie_populatie(pop, dim, n, c, v, cmax, 0.2, 0.25)
 import numpy
 pop = numpy.asarray(pop)
 pop_m = numpy.asarray(pop_m)
 '''
+
+pop, dim, n , c, v, cmax = gen(30,10)
+pop_m = mutatie_populatie(pop, dim, n, c, v, cmax, 0.2, 0.25)
+pop = numpy.asarray(pop)
+pop_m = numpy.asarray(pop_m)
